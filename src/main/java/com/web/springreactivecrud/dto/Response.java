fd89f1cc7bc67f44c6a83904e0779096d06b7d35
salimals2006 <alsaba.boston@gmail.com>
2023-06-26 10:05:23 -0400

    A simple GET call to get square of a number

---
+++ src/main/java/com/web/springreactivecrud/dto/Response.java
@@ -0,0 +1,21 @@
+package com.web.springreactivecrud.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+@NoArgsConstructor
+public class Response {
+
+  private Date date = new Date();
+  private int output;
+
+  public Response(int output){
+    this.output = output;
+  }
+
+}
